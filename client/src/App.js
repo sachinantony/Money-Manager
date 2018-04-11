328df6d73a92b314cb2692092965940f38abab2c
Sachin Antony <sachinantony007@gmail.com>
2018-04-11 01:15:02 -0400

    Added the homepage html and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import {Bootstrap, Grid, Row, Col} from 'react-bootstrap';
+import { Link, Route, Switch } from 'react-router-dom';
 import './bootstrap.min.css';
 import './App.css';
 import SignInPage from './Components/SignInPage';
+import HomePage from './Components/HomePage';
 class App extends Component {
     render() {
         return (
           <div className = 'App'>
-                <SignInPage/>
+                <Route exact={true} path="/" component={SignInPage}/>
+                <Route path="/home" component={HomePage}/>
           </div>
         );
     }
