a7b799617d8315592ceca3134be370d35471730f
Sachin Antony <sachinantony007@gmail.com>
2018-05-04 14:23:32 -0400

    modified the routing setup

--- client/src/Components/SignInPage.js
+++ client/src/Components/SignInPage.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import React from 'react';
 import axios from "axios/index";
+import  {Redirect} from 'react-router-dom';
 
 class SignInPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoggedIn: false,
+        }
+    }
 
     handleSubmit(e){
+        e.preventDefault();
+        var prev = this;
         console.log('reached');
         console.log(this.refs.password.value);
         axios.post('api/account/signin', {
@@ -12,14 +22,21 @@ class SignInPage extends Component {
         })
             .then(function (response) {
                 console.log(response);
+                if(response.data.success === true) {
+                    prev.setState({
+                        isLoggedIn: true
+                    });
+                }
             })
             .catch(function (error) {
                 console.log(error);
             });
-        e.preventDefault();
     }
 
     render(){
+        if(this.state.isLoggedIn === true){
+            return(<Redirect to='/home' />);
+        }
         return(
             <div id="login-overlay" className="modal-dialog vertical-center">
                 <div className="modal-content">
@@ -29,17 +46,17 @@ class SignInPage extends Component {
                     </div>
                     <div className="modal-body">
                         <div className="row">
-                            <div className="col-xs-6">
-                                <div className="well">
-                                    <form onSubmit = {this.handleSubmit.bind(this)} id="loginForm" >
-                                        <div className="form-group">
+                            <form onSubmit = {this.handleSubmit.bind(this)}>
+                                <div className="col-xs-6">
+                                    <div className="well">
+                                        <div>
                                             <label  className="control-label">Username</label>
-                                            <input type="text" className="form-control" ref="email" id="username" name="username"  required="" title="Please enter you username" placeholder="example@gmail.com"/>
+                                            <input type="text"  ref="email" id="username" name="username"  required="" title="Please enter you username" placeholder="example@gmail.com"/>
                                             <span className="help-block"></span>
                                         </div>
-                                        <div className="form-group">
+                                        <div >
                                             <label  className="control-label" >Password</label>
-                                            <input type="password" className="form-control" ref="password" id="password" name="password"  required="" title="Please enter your password"/>
+                                            <input type="password"  ref="password" id="password" name="password"  required="" title="Please enter your password"/>
                                             <span className="help-block"></span>
                                         </div>
                                         <div id="loginErrorMsg" className="alert alert-error hide">Wrong username og password</div>
@@ -49,23 +66,23 @@ class SignInPage extends Component {
                                             </label>
                                             <p className="help-block">(if this is a private computer)</p>
                                         </div>
-                                        <button type="submit" className="btn btn-success btn-block">Login</button>
+                                        <button type="Submit"  className="btn btn-success btn-block">Login</button>
                                         <a href="/forgot/" className="btn btn-default btn-block">Help to login</a>
-                                    </form>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-xs-6">
-                                <p className="lead">Register now for <span className="text-success">FREE</span></p>
-                                <ul className="list-unstyled" style={{lineHeight: 2}}>
-                                    <li><span className="fa fa-check text-success"></span> Track all your expenses</li>
-                                    <li><span className="fa fa-check text-success"></span> Have multiple accounts</li>
-                                    <li><span className="fa fa-check text-success"></span> Create custom budgets</li>
-                                    <li><span className="fa fa-check text-success"></span> Customized reports</li>
-                                    <li><span className="fa fa-check text-success"></span> Enjoy premium for one week <small>(only new customers)</small></li>
-                                    <li><a href="/read-more/"><u>Read more</u></a></li>
-                                </ul>
-                                <p><a href="/new-customer/" className="btn btn-info btn-block">Yes please, register now!</a></p>
-                            </div>
+                                <div className="col-xs-6">
+                                    <p className="lead">Register now for <span className="text-success">FREE</span></p>
+                                    <ul className="list-unstyled" style={{lineHeight: 2}}>
+                                        <li><span className="fa fa-check text-success"></span> Track all your expenses</li>
+                                        <li><span className="fa fa-check text-success"></span> Have multiple accounts</li>
+                                        <li><span className="fa fa-check text-success"></span> Create custom budgets</li>
+                                        <li><span className="fa fa-check text-success"></span> Customized reports</li>
+                                        <li><span className="fa fa-check text-success"></span> Enjoy premium for one week <small>(only new customers)</small></li>
+                                        <li><a href="/read-more/"><u>Read more</u></a></li>
+                                    </ul>
+                                    <p><a href="/new-customer/" className="btn btn-info btn-block">Yes please, register now!</a></p>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter history={createBrowserHistory()}>
     <App />
     </BrowserRouter>
     , document.getElementById('root'));
